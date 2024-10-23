46a456ed657d7fb8301a8104f542c902658306b8
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-10-23 10:49:29 +0100

    Scoring and answer checking

--- main.py
+++ main.py
@@ -12,4 +12,8 @@ for question in data.question_data:
 
 quiz = quiz_brain.QuizBrian(question_bank)
 
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz. Well Done!")
+print(f"Your final score was {quiz.score}/{quiz.question_number}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,18 +3,25 @@ class QuizBrian:
         self.question_number = 0
         self.question_list = q_list
         self.score = 0
-        self.is_correct = True
 
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+        
     def next_question(self):
-        while self.is_correct:
             current_question = self.question_list[self.question_number]
-            answer = input(f"Q. {self.question_number + 1}: {current_question.text} (True/False): ")
+            u_answer = input(f"Q.{self.question_number + 1}: {current_question.text} (True/False)?: ")
+            self.check_answer(u_answer, current_question.answer)
 
-            if answer == current_question.answer:
-                self.score += 1
-                self.question_number += 1
-            else:
-                self.is_correct = False
+    def check_answer(self, u_answer, q_answer):
+        if u_answer.lower() == q_answer.lower():
+             print("You got it right!")
+             self.score += 1
+        else:
+             print("That's wrong.")
+        self.question_number += 1
+        print(f"The correct answer was {q_answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}")
+        print("\n")
 
         
         
