eb354128c44de6ba7afff444b53da0386e1796f6
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2024-10-23 10:22:44 +0100

    Scoring and Answer Checking

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,10 +2,20 @@ class QuizBrian:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
+        self.is_correct = True
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
-        input(f"{self.question_number}: {current_question.text} (True/False): ")
+        while self.is_correct:
+            current_question = self.question_list[self.question_number]
+            answer = input(f"Q. {self.question_number + 1}: {current_question.text} (True/False): ")
+
+            if answer == current_question.answer:
+                self.score += 1
+                self.question_number += 1
+            else:
+                self.is_correct = False
+
         
         
     
